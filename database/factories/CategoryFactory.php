29d1269a6cf03eef80fda8a8b022a60c4e1ece06
Shift <shift@laravelshift.com>
2022-11-20 00:53:41 +0000

    Remove unnecessary `$model` property

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -9,13 +9,6 @@ use Illuminate\Support\Str;
 class CategoryFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Category::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
